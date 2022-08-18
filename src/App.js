ae9109ffba155373f3164f92131eed148ae6cc60
tanujaKaushik <97330627+tanujaKaushik@users.noreply.github.com>
2022-08-18 22:13:49 +0530

    code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 // import logo from './logo.svg';
 import './App.css';
-import Ad_card from './components/AdCard';
-import Filter_box from './components/FilterBox';
 import Navbar from './components/Navbar';
+import HomePage from './components/HomePage';
+import SignUpPage from './components/SignUpPage';
+import DashboardPage from './components/DashboardPage';
+import ExplorePage from './components/ExplorePage';
+import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
 
 function App() {
   return (
     <>
+    <Router>
     <Navbar/>
-    <div className='main mx-20 my-10 flex'>
-      <div className='mx-40 py-4 px-8 mt-20 border-2 bg-blue-100 rounded-lg h-96'>
-        <Filter_box/>
-        </div>
-      <div>
-        <Ad_card image='avatar.png' name='Tanuja Kaushik' address='Shastri Nagar' city='Bahadurgarh' features='fully furnished, ac, near to market'/>
-        <Ad_card image='logo192.png' name='Akanksha' address='Shastri Nagar' city='Manesar' features='fully furnished, ac, near to market'/>
-        <Ad_card image='avatar.png' name='Nikita' address='Shastri Nagar' city='Jhajjar' features='fully furnished, ac, near to market'/>
-        <Ad_card image='logo192.png' name='Akhila' address='Shastri Nagar' city='Bahadurgarh' features='fully furnished, ac, near to market'/>
-      </div>
-    </div>
+     <Routes>
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/explore" element={<ExplorePage />} />
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+    </Router>
     </>
   );
 }
